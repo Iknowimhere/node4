2c00d0ec4e8b4b29088aabf95d9925d358a07787
umashankar <umashankarp33@gmail.com>
2025-08-11 17:25:20 +0530

    implement update and delete

--- todo-app/frontend/src/App.jsx
+++ todo-app/frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from "react"
-import TodoInput from "./components/TodoInput"
-import TodoList from "./components/TodoList"
+import { useState } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 const App = () => {
   let [task,setTask]=useState("")
     let [todos, setTodos] = useState([]);
     let [loading,setLoading]=useState(false);
     let [error,setError]=useState("")
+
   return (
     <>
-    <TodoInput task={task} setTask={setTask} setTodos={setTodos} loading={loading} setLoading={setLoading}/>
-    <TodoList todos={todos} setTodos={setTodos} loading={loading} setLoading={setLoading} error={error} setError={setError}/>
+      <TodoInput task={task} setTask={setTask} setTodos={setTodos} loading={loading} setLoading={setLoading}/>
+      <TodoList todos={todos} setTodos={setTodos} loading={loading} setLoading={setLoading} error={error} setError={setError}/>
     </>
-  )
-}
-export default App
+  );
+};
+export default App;
--- todo-app/frontend/src/components/TodoList.jsx
+++ todo-app/frontend/src/components/TodoList.jsx
@@ -1,20 +1,29 @@
 import { useEffect, useState } from "react";
 import axios from "../axios";
 
-const TodoList = ({todos,setTodos,setLoading,loading,setError,error}) => {
+const TodoList = ({
+  todos,
+  setTodos,
+  setLoading,
+  loading,
+  setError,
+  error,
+}) => {
+  let [editId,setEditId]=useState(null)
+  let [editTask,setEditTask]=useState("")
 
   const fetchTodos = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
       let res = await axios.get("/");
       setTodos(res.data);
-      setLoading(false)
+      setLoading(false);
     } catch (error) {
       console.log(error);
-      if(error.status===500){
-        setError("Internal server error")
+      if (error.status === 500) {
+        setError("Internal server error");
       }
-      setLoading(false)
+      setLoading(false);
     }
   };
 
@@ -22,13 +31,15 @@ const TodoList = ({todos,setTodos,setLoading,loading,setError,error}) => {
     fetchTodos();
   }, []);
 
-  if(error){
-    return <p style={{color:"red"}}>{error}</p>
+  if (error) {
+    return <p style={{ color: "red" }}>{error}</p>;
   }
 
   const handleToggle = async (todo) => {
     try {
-      let res = await axios.put(`/status/${todo._id}`, { completed: !todo.completed });
+      let res = await axios.put(`/status/${todo._id}`, {
+        completed: !todo.completed,
+      });
       setTodos((prevTodos) =>
         prevTodos.map((t) =>
           t._id === todo._id ? { ...t, completed: !t.completed } : t
@@ -37,20 +48,55 @@ const TodoList = ({todos,setTodos,setLoading,loading,setError,error}) => {
     } catch (error) {
       console.log(error);
     }
-  }  
+  };
+
+  const handleDelete=async (id)=>{
+      await axios.delete(`/${id}`);
+      setTodos((prevTodos)=>prevTodos.filter(t=>t._id!==id))
+  }
+
+  const handleEdit=(todo)=>{
+    setEditId(todo._id)
+    setEditTask(todo.task)
+  }
+
+
+  const handleUpdate=async()=>{
+    let res=await axios.put(`/${editId}`,{task:editTask})
+     setTodos((prevTodos) =>
+        prevTodos.map((t) =>
+          t._id === editId ? { ...t, task:editTask } : t
+        )
+      );
+    setEditId(null)
+  }
   return (
     <ul>
-      {loading?(<p>Loading...</p>):(<>{todos.map((todo) => {
-        return (
-          <li key={todo._id}>
-            <span>{todo.task}</span>---
-            <span>{todo.completed ? "Done!!" : "Not done!!"}</span>
-            <button onClick={()=>handleToggle(todo)}>Toggle status</button>
-            <button>Edit</button>
-            <button>Delete</button>
-          </li>
-        );
-      })}</>)}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {todos.length>0 && todos.map((todo) => {
+            return (
+              <li key={todo._id}>
+               {editId===todo._id?(<>
+                <input type="text" name="task" value={editTask} onChange={(e)=>setEditTask(e.target.value)}/>
+                <button onClick={handleUpdate}>Update</button>
+                <button onClick={()=>setEditId(null)}>Cancel</button>
+               </>):(<>
+                <span>{todo.task}</span>---
+                <span>{todo.completed ? "Done!!" : "Not done!!"}</span>
+                <button onClick={() => handleToggle(todo)}>
+                  Toggle status
+                </button>
+                <button onClick={()=>handleEdit(todo)}>Edit</button>
+                <button onClick={() => handleDelete(todo._id)}>Delete</button>
+               </>)}
+              </li>
+            );
+          })}
+        </>
+      )}
     </ul>
   );
 };
