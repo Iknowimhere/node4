1db7610bfe731928ba2520b4505f0e6c75bcdba5
umashankar <umashankarp33@gmail.com>
2025-08-15 16:27:44 +0530

    add login

--- auth/backend/src/controllers/auth.controllers.js
+++ auth/backend/src/controllers/auth.controllers.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
 
 let register=async (req,res,next)=>{
     let {username,email,password,confirmPassword}=req.body
@@ -14,17 +15,34 @@ let register=async (req,res,next)=>{
             password,
             confirmPassword
         })
-        res.status(200).json(newUser)
+        res.status(200).json({username:newUser.username,email:newUser.email})
     } catch (error) {
         res.status(500).json({message:error.message})
     }
 }
 
-let login=(req,res,next)=>{
-    //TODO: check user is there or not
-    //TODO:if user is not there ask him to register first
-    //TODO: match password with hashed password in db
-    //TODO: return user if password is a match
+let login=async(req,res,next)=>{
+    let {email,password}=req.body
+    try{
+        //TODO: check user is there or not
+        let existingUser=await User.findOne({email})
+        //TODO:if user is not there ask him to register first
+        if(!existingUser){
+            res.status(400).json({message:"User doesnt exist please signup"})
+            return;
+        }
+        //TODO: match password with hashed password in db
+        let isMatch=await bcrypt.compare(password,existingUser.password)
+
+        if(!isMatch){
+            res.status(400).json({message:"Password dosent match!!"})
+            return; 
+        }
+        //TODO: return user if password is a match
+        res.status(200).json({username:existingUser.username,email:existingUser.email})
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
 }
 
 export {
